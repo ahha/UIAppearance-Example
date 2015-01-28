25524ce4d959edfb0322bb45bf855fc6def41cd2
Andrew Carter <andrew.r.carter@gmail.com>
2015-01-27 22:37:02 -0500

    first commit

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -15,30 +15,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.applyTheme()
+        
         return true
     }
 
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    func applyTheme() {
+        
+        // Make all navigation bars in the app purple
+        UINavigationBar.appearance().barTintColor = UIColor.purpleColor();
+        
+        // Change tint color of navigation bars
+        UINavigationBar.appearance().tintColor = UIColor.yellowColor()
+       
+        // Use a custom font for bar button items
+        UIBarButtonItem.appearance().setTitleTextAttributes(
+            [NSFontAttributeName : UIFont(name: "Chalkduster", size: 17.0)!],
+            forState: UIControlState.Normal)
+       
+        // Custom back icon
+        UINavigationBar.appearance().backIndicatorImage = UIImage(named: "back");
+        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "back");
+        
+        // Adjust the back text for our new image
+        UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffset(horizontal: 10.0, vertical: 0.0), forBarMetrics: .Default);
     }
 
 
